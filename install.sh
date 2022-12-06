c777709df23a7e3ad125042d5464603a9f4063e4
Bruno Almeida <brunoslalmeida@gmail.com>
2022-12-06 19:11:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
-microk8s enable nginx 
+#!/bin/bash
+
+echo "enbling ingress"
+microk8s enable ingress 
 
 #install cert-manager
+echo "installing cert-manager"
 microk8s.kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml
 microk8s.kubectl apply -f ./cert-manager/cluster-issuer-prod.yaml
 
 #install application
+echo "deploying application"
 microk8s.kubectl apply -f ./strapi/namespace.yaml
 microk8s.kubectl apply -f ./strapi/service.yaml
 microk8s.kubectl apply -f ./strapi/ingress.yaml
 microk8s.kubectl apply -f ./strapi/deployment.yaml
+
+# Documentation
